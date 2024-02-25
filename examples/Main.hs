31b42ceb2e99eac622e642a8f00c172ba63d9504
Ofsouzap <ofsouzap@gmail.com>
2024-02-25 09:40:51 +0000

    NNF propositions

--- examples/Main.hs
+++ examples/Main.hs
@@ -5,13 +5,19 @@ import Proving.Propositions
 
 main :: IO ()
 main = do
-  let p = Impl (And (Atom (Var "P") :| [Atom (Var "Q")])) (Not (Atom (Var "Q")))
+  let p = PropImpl (PropAnd (PropAtom (AtomVar "P") :| [PropAtom (AtomVar "Q")])) (PropNot (PropAtom (AtomVar "Q")))
   print p
   let env1 = createEnv [("P", True), ("Q", False)]
+  print "Eval {P, !Q}:"
   (print . evaluateProposition env1) p
   let env2 = createEnv [("P", True), ("Q", True)]
+  print "Eval {P, Q}:"
   (print . evaluateProposition env2) p
   let env3 = createEnv [("P", False)]
+  print "Eval {!P}:"
   (print . evaluateProposition env3) p
   let env4 = createEnv [("P", True)]
+  print "Eval {P}:"
   (print . evaluateProposition env4) p
+  print "Nnf:"
+  (print . propositionToNnfProposition) p
--- src/Proving/Propositions.hs
+++ src/Proving/Propositions.hs
@@ -5,7 +5,10 @@ module Proving.Propositions
   , envEval
   , Atom(..)
   , Proposition(..)
-  , evaluateProposition ) where
+  , evaluateProposition
+  , NegAtom(..)
+  , NnfProposition(..)
+  , propositionToNnfProposition ) where
 
 import Data.List.NonEmpty
   ( NonEmpty((:|)) )
@@ -37,50 +40,106 @@ envEval var (Env xs) =
 
 -- | A propositional atom
 data Atom
-  = Lit Bool
-  | Var Varname
+  = AtomLit Bool
+  | AtomVar Varname
   deriving ( Eq )
 
 instance Show Atom where
-  show (Lit True) = "t"
-  show (Lit False) = "f"
-  show (Var var) = var
+  show (AtomLit True) = "t"
+  show (AtomLit False) = "f"
+  show (AtomVar var) = var
 
 -- | A proposition of propositional logic
 data Proposition
-  = Atom Atom
-  | Not Proposition
-  | Or (NonEmpty Proposition)
-  | And (NonEmpty Proposition)
-  | Impl Proposition Proposition
-  | BiImpl Proposition Proposition
+  = PropAtom Atom
+  | PropNot Proposition
+  | PropOr (NonEmpty Proposition)
+  | PropAnd (NonEmpty Proposition)
+  | PropImpl Proposition Proposition
+  | PropBiImpl Proposition Proposition
 
 instance Show Proposition where
-  show (Atom a) = show a
-  show (Not p) = "¬" ++ (bracketWrap . show) p
-  show (Or ps) = (intercalate "+" . NonEmpty.toList .fmap (bracketWrap . show)) ps
-  show (And ps) = (intercalate "." . NonEmpty.toList . fmap (bracketWrap . show)) ps
-  show (Impl a b) = (bracketWrap . show) a ++ "->" ++ (bracketWrap . show) b
-  show (BiImpl a b) = (bracketWrap . show) a ++ "<->" ++ (bracketWrap . show) b
+  show (PropAtom a) = show a
+  show (PropNot p) = "¬" ++ (bracketWrap . show) p
+  show (PropOr ps) = (intercalate "+" . NonEmpty.toList .fmap (bracketWrap . show)) ps
+  show (PropAnd ps) = (intercalate "." . NonEmpty.toList . fmap (bracketWrap . show)) ps
+  show (PropImpl a b) = (bracketWrap . show) a ++ "->" ++ (bracketWrap . show) b
+  show (PropBiImpl a b) = (bracketWrap . show) a ++ "<->" ++ (bracketWrap . show) b
 
 -- | Evaluate a proposition. If fails to evaluate due to not having a valuation for a variable then returns Nothing.
 -- Due to how evaluation is performed, this doesn't always detect variables without valuations and will sometimes return a
 -- result if one can be determined without evaluating some variables.
 evaluateProposition :: Env -> Proposition -> Maybe Bool
-evaluateProposition _ (Atom (Lit b)) = Just b
-evaluateProposition env (Atom (Var var)) = envEval var env
-evaluateProposition env (Not p) = not <$> evaluateProposition env p
-evaluateProposition env (Or ps) = foldr foldFunc (Just False) ps where
+evaluateProposition _ (PropAtom (AtomLit b)) = Just b
+evaluateProposition env (PropAtom (AtomVar var)) = envEval var env
+evaluateProposition env (PropNot p) = not <$> evaluateProposition env p
+evaluateProposition env (PropOr ps) = foldr foldFunc (Just False) ps where
   foldFunc :: Proposition -> Maybe Bool -> Maybe Bool
   foldFunc p next = case evaluateProposition env p of
     Nothing -> Nothing
     Just True -> Just True
     Just False -> next
-evaluateProposition env (And ps) = foldr foldFunc (Just True) ps where
+evaluateProposition env (PropAnd ps) = foldr foldFunc (Just True) ps where
   foldFunc :: Proposition -> Maybe Bool -> Maybe Bool
   foldFunc p next = case evaluateProposition env p of
     Nothing -> Nothing
     Just False -> Just False
     Just True -> next
-evaluateProposition env (Impl a b) = evaluateProposition env (Or (Not a :| [b]))
-evaluateProposition env (BiImpl a b) = evaluateProposition env (Or (And (a :| [b]) :| [And (Not a :| [Not b])]))
+evaluateProposition env (PropImpl a b) = evaluateProposition env (PropOr (PropNot a :| [b]))
+evaluateProposition env (PropBiImpl a b) = evaluateProposition env (PropOr (PropAnd (a :| [b]) :| [PropAnd (PropNot a :| [PropNot b])]))
+
+-- NNF
+
+-- | Propositional logic proposition without implication
+data SimpleProposition
+  = SPAtom Atom
+  | SPNot SimpleProposition
+  | SPOr (NonEmpty SimpleProposition)
+  | SPAnd (NonEmpty SimpleProposition)
+
+propositionToSimpleProposition :: Proposition -> SimpleProposition
+propositionToSimpleProposition (PropAtom a) = SPAtom a
+propositionToSimpleProposition (PropNot p) = (SPNot . propositionToSimpleProposition) p
+propositionToSimpleProposition (PropOr ps) = (SPOr . fmap propositionToSimpleProposition) ps
+propositionToSimpleProposition (PropAnd ps) = (SPAnd . fmap propositionToSimpleProposition) ps
+propositionToSimpleProposition (PropImpl a b) = SPOr ((SPNot . propositionToSimpleProposition) a :| [propositionToSimpleProposition b])
+propositionToSimpleProposition (PropBiImpl a b) = SPOr
+  ( SPAnd (propositionToSimpleProposition a :| [propositionToSimpleProposition b])
+  :| [SPAnd ((SPNot . propositionToSimpleProposition) a :| [(SPNot . propositionToSimpleProposition) b])] )
+
+-- | Atomic value containing negation information
+data NegAtom
+  = NegAtomLit Bool
+  | NegAtomVar Varname
+  | NegAtomNegVar Varname
+
+atomToNegAtom :: Atom -> NegAtom
+atomToNegAtom (AtomLit b) = NegAtomLit b
+atomToNegAtom (AtomVar var) = NegAtomVar var
+
+-- | Propositional logic proposition in NNF (Negated Normal Form)
+data NnfProposition
+  = NnfAtom NegAtom
+  | NnfOr (NonEmpty NnfProposition)
+  | NnfAnd (NonEmpty NnfProposition)
+
+instance Show NnfProposition where
+  show (NnfAtom (NegAtomLit b)) = show b
+  show (NnfAtom (NegAtomVar var)) = var
+  show (NnfAtom (NegAtomNegVar var)) = "¬" ++ show var
+  show (NnfOr ps) = (intercalate "+" . NonEmpty.toList .fmap (bracketWrap . show)) ps
+  show (NnfAnd ps) = (intercalate "." . NonEmpty.toList . fmap (bracketWrap . show)) ps
+
+simplePropositionToNnfProposition :: SimpleProposition -> NnfProposition
+simplePropositionToNnfProposition (SPAtom a) = (NnfAtom . atomToNegAtom) a
+simplePropositionToNnfProposition (SPOr ps) = (NnfOr . fmap simplePropositionToNnfProposition) ps
+simplePropositionToNnfProposition (SPAnd ps) = (NnfAnd . fmap simplePropositionToNnfProposition) ps
+simplePropositionToNnfProposition (SPNot (SPNot p)) = simplePropositionToNnfProposition p
+simplePropositionToNnfProposition (SPNot (SPAtom (AtomLit b))) = (NnfAtom . NegAtomLit . not) b
+simplePropositionToNnfProposition (SPNot (SPAtom (AtomVar var))) = (NnfAtom . NegAtomNegVar) var
+simplePropositionToNnfProposition (SPNot (SPOr ps)) = (NnfAnd . fmap (simplePropositionToNnfProposition . SPNot)) ps
+simplePropositionToNnfProposition (SPNot (SPAnd ps)) = (NnfOr . fmap (simplePropositionToNnfProposition . SPNot)) ps
+
+-- | Convert a raw proposition to a proposition in NNF (Negated Normal Form)
+propositionToNnfProposition :: Proposition -> NnfProposition
+propositionToNnfProposition = simplePropositionToNnfProposition . propositionToSimpleProposition
